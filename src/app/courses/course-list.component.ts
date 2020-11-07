a723e7fa1812dfac40b852256eeae2cc2da358f5
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-07 13:59:51 -0300

    add course service

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,47 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
 
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+    _courses: Course[] = [];
+    _filterBy: string;
+
+    constructor(private courseService: CourseService) {}
 
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: "Angular 2",
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'ABC-123',
-                duration: 120,
-                rating: 3,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: "Postgress",
-                imageUrl: '/assets/images/http.png',
-                price: 30.99,
-                code: 'ABC-123',
-                duration: 120,
-                rating: 5,
-                releaseDate: 'December, 4, 2019'
-            },
-            {
-                id: 3,
-                name: "Postgress",
-                imageUrl: '/assets/images/http.png',
-                price: 30.99,
-                code: 'ABC-123',
-                duration: 120,
-                rating: 3.5,
-                releaseDate: 'December, 4, 2019'
-            },
-        ]
+        this._courses = this.courseService.retriveAll();
+        this.filteredCourses = this._courses;
     }
 
+    set filter(value: string) {
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) =>
+            course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1
+        );
+    }
+
+    get filter() {
+        return this._filterBy;
+    }
 }
---
+++ src/app/courses/course.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { Course } from './course';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CourseService {
+
+    retriveAll(): Course[] {
+        return COURSES;
+    }
+}
+
+var COURSES: Course[] = [
+    {
+        id: 1,
+        name: "Angular 2",
+        imageUrl: '/assets/images/forms.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 3,
+        releaseDate: 'December, 2, 2019'
+    },
+    {
+        id: 2,
+        name: "Angular 3",
+        imageUrl: '/assets/images/cli.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 5,
+        releaseDate: 'December, 3, 2019'
+    },
+    {
+        id: 3,
+        name: "Angular 4",
+        imageUrl: '/assets/images/router.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 1,
+        releaseDate: 'December, 4, 2019'
+    },
+    {
+        id: 4,
+        name: "Angular 2",
+        imageUrl: '/assets/images/forms.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 2.5,
+        releaseDate: 'December, 2, 2019'
+    },
+    {
+        id: 5,
+        name: "Angular 3",
+        imageUrl: '/assets/images/cli.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 4,
+        releaseDate: 'December, 3, 2019'
+    },
+    {
+        id: 6,
+        name: "Angular 4",
+        imageUrl: '/assets/images/router.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 3,
+        releaseDate: 'December, 4, 2019'
+    },
+    {
+        id: 7,
+        name: "Angular 2",
+        imageUrl: '/assets/images/forms.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 5,
+        releaseDate: 'December, 2, 2019'
+    },
+    {
+        id: 8,
+        name: "Angular 3",
+        imageUrl: '/assets/images/cli.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 3,
+        releaseDate: 'December, 3, 2019'
+    },
+    {
+        id: 9,
+        name: "Angular 4",
+        imageUrl: '/assets/images/router.png',
+        price: 99.99,
+        code: 'ABC-123',
+        duration: 120,
+        description: "Nesse curso os alunos irão aprender um grande conhecimento nos principios basicos",
+        rating: 3,
+        releaseDate: 'December, 4, 2019'
+    }
+];
