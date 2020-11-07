575061d085c1189275d8f396b2bd127e9376f467
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-07 14:00:43 -0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    NavBarComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
